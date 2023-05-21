6b3808c638233d3655423ba09957e25ab3b6eadb
santhoshmlops <santhoshmlops@gmail.com>
2023-05-21 19:13:36 +0530

    pipeline completed

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.pipeline.train_pipeline import TrainPipeline
+
+train_pipeline = TrainPipeline()
---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,21 @@
+from src.components.data_ingestion import DataIngestion
+from src.components.data_ingestion import DataIngestionConfig
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
+
+class TrainPipeline:
+    def __init__(self):
+        obj=DataIngestion()
+        train_data,test_data=obj.initiate_data_ingestion()
+
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+        modeltrainer=ModelTrainer()
+        print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
